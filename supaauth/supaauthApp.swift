a11035840e77fcb56381ff990a361bf92f682de2
egor-romanov <egor.romanov@gmail.com>
2024-04-16 11:08:24 +0100

    Initial Commit

---
+++ supaauth/supaauthApp.swift
@@ -0,0 +1,17 @@
+//
+//  supaauthApp.swift
+//  supaauth
+//
+//  Created by Egor Romanov on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct supaauthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
